fb0635a81661b80d39d344ac9257c56ec91d5f78
Mochacola <54413553+Mochacola500@users.noreply.github.com>
2023-12-29 08:47:07 +0900

    Add json export type testcase

--- ExcelExport.Test/SerializeTest.cs
+++ ExcelExport.Test/SerializeTest.cs
@@ -4,24 +4,33 @@ namespace ExcelExport.Test
     [TestFixture]
     public class SerializeTest
     {
-        ExcelExport m_Export;
+        ExcelExport m_SimpleData;
+        Dictionary<string, string> m_JsonData;
 
         [SetUp]
         public void Init()
         {
-            m_Export = ExcelExport.Load("../../../SimpleData.xlsx");
+            m_SimpleData = Load("SimpleData.xlsx");
+            using var jsonData = Load("JsonData.xlsx");
+            m_JsonData = jsonData.ToJson().ToDictionary(x => x.Name, x => x.Json);
+        }
+
+        ExcelExport Load(string path)
+        {
+            var fullPath = Path.Combine("../../../Data", path);
+            return ExcelExport.Load(fullPath);
         }
 
         [TearDown]
         public void CleanUp()
         {
-            m_Export.Dispose();
+            m_SimpleData.Dispose();
         }
 
         [Test]
         public void CSVTest1()
         {
-            var csvResult = m_Export.ToCSV().First();
+            var csvResult = m_SimpleData.ToCSV().First();
             string expected = "a,b,c,d,e\r\n1,2,3,4,5\r\n1,2,3,4,5\r\n1,2,3,4,5\r\n1,2,3,4,5";
             Assert.That(csvResult.CSV, Is.EqualTo(expected));
         }
@@ -29,9 +38,30 @@ namespace ExcelExport.Test
         [Test]
         public void JsonTest1()
         {
-            var jsonResult = m_Export.ToJson().First();
+            var jsonResult = m_SimpleData.ToJson().First();
             string expected = "[{\"a\":\"1\",\"b\":\"2\",\"c\":\"3\",\"d\":\"4\",\"e\":\"5\"},{\"a\":\"1\",\"b\":\"2\",\"c\":\"3\",\"d\":\"4\",\"e\":\"5\"},{\"a\":\"1\",\"b\":\"2\",\"c\":\"3\",\"d\":\"4\",\"e\":\"5\"},{\"a\":\"1\",\"b\":\"2\",\"c\":\"3\",\"d\":\"4\",\"e\":\"5\"}]";
             Assert.That(jsonResult.Json, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void MemberJsonTest()
+        {
+            var expected = "{\"a\":\"1\",\"b\":\"2\",\"c\":\"3\",\"d\":\"4\",\"e\":\"5\"}";
+            Assert.That(m_JsonData["MemberData"], Is.EqualTo(expected));  
+        }
+
+        [Test]
+        public void ListJsonTest()
+        {
+            var expected = "[{\"a\":\"1\",\"b\":\"2\",\"c\":\"3\",\"d\":\"4\",\"e\":\"5\"},{\"a\":\"2\",\"b\":\"3\",\"c\":\"4\",\"d\":\"5\",\"e\":\"6\"}]";
+            Assert.That(m_JsonData["ListData"], Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void DictionaryJsonTest()
+        {
+            var expected = "\"1\":{\"id\":\"1\",\"b\":\"2\",\"c\":\"3\",\"d\":\"4\",\"e\":\"5\"},\"2\":{\"id\":\"2\",\"b\":\"3\",\"c\":\"4\",\"d\":\"5\",\"e\":\"6\"}";
+            Assert.That(m_JsonData["DictionaryData"], Is.EqualTo(expected));
         }
     }
 }
